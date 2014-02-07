cca2c105a08dc635e047a78aaaeda74deb47aba1
Pierre Tachoire <ptachoire@itnetwork.fr>
2014-02-07 10:12:32 +0100

    remove dead code

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('co_g_stupid_mq');
+        $treeBuilder->root('co_g_stupid_mq');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
