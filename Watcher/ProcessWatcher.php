17bb18e3b2550870c3e3ad9398d87d87dab8178e
Pierre Tachoire <ptachoire@itnetwork.fr>
2014-07-25 11:49:05 +0200

    add a way to run processes in parallel

--- Watcher/ProcessWatcher.php
+++ Watcher/ProcessWatcher.php
@@ -11,6 +11,7 @@ use CoG\StupidMQ\Queue\QueueInterface;
 use CoG\StupidMQ\Exception\NoResultException;
 use Symfony\Component\Process\Exception\RuntimeException;
 use Symfony\Component\Process\Process;
+use CoG\StupidMQ\Message\MessageInterface;
 
 /**
  * LoopWatcher
@@ -22,11 +23,13 @@ class ProcessWatcher extends AbstractWatcher
     protected $queues;
     protected $cwd;
     protected $command;
+    protected $processes;
 
     public function __construct( $cwd, $command ) {
         $this->queues = array();
         $this->command = $command;
         $this->cwd = $this->cwd;
+        $this->processes = array();
     }
 
     public function addQueue( QueueInterface $queue ) {
@@ -49,19 +52,29 @@ class ProcessWatcher extends AbstractWatcher
             }
 
             if($this->shouldStop($opt)) {
+                $this->processLoop($wait = true);
                 break;
             }
 
             if( $wait ) {
                 sleep($opt['sleep']);
             }
+
+            $this->processLoop();
+
         } while( true );
     }
 
     protected function consume( QueueInterface $queue, array $opt ) {
         try {
+            while ($opt['max-process'] > 0 &&
+                count($this->processes) >= $opt['max-process']
+            ) {
+                $this->processLoop();
+                usleep(1000);
+            }
+
             $message = $queue->consume();
-            $this->log(sprintf('Run message %s found in queue %s', $message->getId(), $queue->getName()), 'info');
             $process = new Process(
                 sprintf(
                     $this->command,
@@ -75,17 +88,23 @@ class ProcessWatcher extends AbstractWatcher
             );
 
             $logger = $this;
-            $process->run(function ($type, $buffer) use ($logger) {
-                if ('err' === $type) {
-                    $logger->log($buffer, 'err');
-                } else {
-                    $logger->log($buffer);
-                }
-            });
+            $process->start();
 
-            return true;
-        } catch ( RuntimeException $ex ) {
-            $this->log(sprintf('Timeout in queue %s', $queue->getName()), 'err');
+            $this->log(
+                sprintf(
+                    'Run message %s found in queue %s',
+                    $message->getId(),
+                    $queue->getName()
+                ),
+                'info'
+            );
+
+            $this->processes[] = array(
+                'process' => $process,
+                'message' => $message,
+                'queue' => $queue
+            );
+
             return true;
         } catch ( NoResultException $ex ) {
             $this->log(sprintf('No message found in queue %s', $queue->getName()), 'debug');
@@ -93,4 +112,45 @@ class ProcessWatcher extends AbstractWatcher
         }
     }
 
+    protected function processLoop($wait = false)
+    {
+        foreach ($this->processes as $key => $data) {
+            $process = $data['process'];
+            $message = $data['message'];
+            $queue = $data['queue'];
+            try {
+                if (!$process->isRunning()) {
+                    $stdout = $process->getOutput();
+                    if (!empty($stdout)) {
+                        $this->log($stdout, 'info');
+                    }
+                    $stderr = $process->getErrorOutput();
+                    if (!empty($stderr)) {
+                        $this->log($stderr, 'err');
+                    }
+                    unset($this->processes[$key]);
+                    $this->log(sprintf('end message %s', $message->getId()), 'debug');
+                } elseif ($wait) {
+                    $logger = $this;
+                    $process->wait(function ($type, $buffer) use ($logger) {
+                        if ('err' === $type) {
+                            $logger->log($buffer, 'err');
+                        } else {
+                             $logger->log($buffer, 'info');
+                        }
+                    });
+                    unset($this->processes[$key]);
+                    $this->log(sprintf('end message %s', $message->getId()), 'debug');
+                } else {
+                    $process->checkTimeout();
+                }
+            } catch ( RuntimeException $ex ) {
+                unset($this->processes[$key]);
+                $queue->feedback($message->getId(), MessageInterface::STATE_ERROR, $ex->getMessage());
+                $this->log(sprintf('Exception %s for message %s', $ex->getMessage(), $message->getId()), 'err');
+            }
+        }
+
+        return count($this->processes);
+    }
 }
